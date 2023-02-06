eb45a7d505de3912fb05ea9c4193b4c6f9ccd8d7
YukiZensho <yukizenshou@gmail.com>
2023-02-06 22:34:42 +0200

    prototype

--- js/map.js
+++ js/map.js
@@ -17,10 +17,11 @@ image.onload = function() {
 
 function getPixelColor(x, y){
     var map = document.getElementById("actMap");
-    x -= map.offsetLeft - map.offsetWidth/2;
-    y -= map.offsetTop;
+    x -= map.offsetLeft - map.offsetWidth/2 - document.documentElement.scrollLeft;
+    y -= map.offsetTop - document.documentElement.scrollTop;
     x = ~~(mapX * x / map.offsetWidth);
     y = ~~(mapY * y / map.offsetHeight);
+    console.log(x + " " + y);
     if(x > 0 && x < mapX && y > 0 && y < mapY)
         return ctx.getImageData(x, y, 1, 1).data;
     else 
@@ -72,10 +73,34 @@ function colorMatch(value){
     if(col[0] == 42 && col[1] == 194 && col[2] ==195 && col[3] > 250 )
         return "Fara";
 }
+function texthing(who){
+    console.log(who);
+    var NewText = "EMPTY";
+
+    if(who == "Hamnq") 
+        NewText = "<h3> HAMNQ </h3> <p> The birth of the Ra'u, hamnq is the place to be if you want to experience the old order Ga, although through the centuries of war it has come to be a bastion of bastions and a castle of castles </p>";
+    if(who == "Hayi") 
+        NewText = "<h3> HAYI </h3> <p> As even their name suggests, hayi is the land of elegance, cotton and the new order Ga, going through Rokya to Kyani you pass through the lands so many of our gods have been through, from Rokyan archipelago, to the BaMiNya river, and to the Ba'u river where the Ba'u script has been birhted </p>";
+    if(who == "Hasi") 
+        NewText = "<h3> HASI </h3> <p> If you want to marvel at the advancements of our world you need to visit Hasi, the land of good, where the cultural mix presents a variety of options, but think wrong, Hasi is still Ha land, so you will be welcomed as a Ga believer, and through thanks to the people of other beliefs you will be able to take comfort in RiMa like nowhere else";
+    if(who == "Hago")
+        NewText = "<h3> HAGO </h3> <p> Hago, as the name implies is the newest Ha lands, this makes it a land of exploration and adventure, it is the perfect land for a person who is ready to help in the expansion of our great religion</p>"
+    if(who == "Hafu")
+        NewText = "<h3> HAFU </h3> <p> Hafu represents a miriad of individual places, adventuring through it is bound to bring a new perspective in life, one of understanding why Ga is the religion it is, despite the fact that most places are old order you would be hard pressed to find more religious variety elsewhere, with multiple sea gods you are always under the protection of good spirits no matter where you decide to venture</p>"
+    if(who != undefined && NewText == "EMPTY") 
+        NewText = "<h3>" + who + "</h3> <p> There is not a description of them yet </p>";
+    else if(who == undefined)
+        NewText = "<h3> EMPTY </h3> <p> Press on any culture to find a short description of them </p>";
+    
+    document.getElementById("context").innerHTML = NewText;
+}
+
 hoverFunc("WRONG");
 
 document.addEventListener("click", function(event) {
     col = getPixelColor(event.clientX, event.clientY);
-    console.log(col);
-    hoverFunc(colorMatch(col));
+    if(col != -1){
+        hoverFunc(colorMatch(col));
+        texthing(colorMatch(col));
+    }
   });

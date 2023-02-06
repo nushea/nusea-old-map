42ceecfb1f019a01fcc4131838ff308644fca3ea
YukiZensho <yukizenshou@gmail.com>
2023-02-06 17:19:03 +0200

    niu

--- js/map.js
+++ js/map.js
@@ -1,4 +1,21 @@
-var Body="";
-
-Body = document.getElementById("map").innerHTML;
-Body+= "<img src=\"img/Nusea/Blank\">"
+function hoverFunc(who, value){
+    switch(value){
+    case 0:
+        document.getElementById(who).style.display = "block";
+        return;
+    case 1:
+        document.getElementById(who).style.display = "none";
+        return;
+    }
+}
+var elements = document.getElementsByClassName("mapimg");
+var elementsArray = Array.from(elements);
+for (var i = 0; i < elementsArray.length; i++) {
+    if(elementsArray[i].id === "blank" || elementsArray[i].id === "empty")
+        elementsArray[i].style.display = "block";
+    else
+        elementsArray[i].style.display = "none";
+        
+  }
+  
+//document.getElementById("blank").style.display = "none";

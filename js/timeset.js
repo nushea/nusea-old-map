5f08cdc64131b0eecf50f87020eb16704229745e
Shea <nu@she-a.eu>
2025-03-02 15:47:32 +0200

    pings every second

--- js/timeset.js
+++ js/timeset.js
@@ -2,22 +2,45 @@
 var script = document.createElement('script');
 script.src = 'js/time.js';
 document.head.appendChild(script);
+var curtime;
+
 
 script.onload = () => {
     Module.onRuntimeInitialized = () => {
-        console.log("wasm loaded!");
-
-        // Wrap the curtime function from C
-		var curtime = Module.cwrap('curtime', null, ['number']);
-		
-		var oup = Module._malloc(50);
-        // Call curtime and log the result
-        console.log("curtime is ", curtime(oup));
-		var outputString = Module.UTF8ToString(oup);
-		console.log(outputString);
-		
-		Module._free(oup);
-
+        
+		curtime = Module.cwrap('curtime', null, ['number']);
     };
 };
+
+//while(curtime == null){
+//	console.log("nowasm");
+//}
+
+
+var checkWasmReady = setInterval(() => {
+    if (curtime) {
+		var oup = Module._malloc(50);
+		curtime(oup);
+		var outputString = Module.UTF8ToString(oup);
+		console.log(outputString);
+	        
+
+        Module._free(oup);
+    } else {
+        console.log("nowasm");
+    }
+}, 1000);
+
+/*
+var oup = Module._malloc(50);
+
+
+    curtime(oup);
+	var outputString = Module.UTF8ToString(oup);
+	console.log(outputString);
+	console.log(outputString);
+	i++;
+		
+	Module._free(oup);
+*/
 
